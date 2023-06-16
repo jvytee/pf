2fb39d6722616f43b8d3774233d54c30c5515cdf
Julian Theis <julian.theis@posteo.de>
2023-06-16 22:57:49 +0200

    Sort dependencies, add package

--- flake.nix
+++ flake.nix
@@ -1,4 +1,8 @@
 {
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+  };
+
   outputs = { self, nixpkgs }: 
   let
     systems = ["x86_64-linux"];
@@ -12,15 +16,15 @@
         name = "pf-shell";
 
         nativeBuildInputs = [
-          check
           clang-tools
-          libpng
-        ];
-
-        buildInputs = [
           meson
           ninja
           pkg-config
+        ];
+
+        buildInputs = [
+          check
+          libpng
         ];
       };
 
@@ -30,6 +34,29 @@
           python39Packages.pillow
           nodepackages.pyright
         ];
+      };
+    });
+
+    packages = genAttrs (system:
+    with import nixpkgs { inherit system; };
+    {
+      pf = clangStdenv.mkDerivation {
+        pname = "pf";
+        version = "0.1.0";
+        src = self;
+
+        nativeBuildInputs = [
+          meson
+          ninja
+          pkg-config
+        ];
+        
+        buildInputs = [
+          check
+          libpng
+        ];
+
+        mesonBuildType= "release";
       };
     });
   };
